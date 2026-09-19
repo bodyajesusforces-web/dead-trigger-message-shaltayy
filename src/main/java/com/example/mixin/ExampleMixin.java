cfbe2784ce26ff2a0148a71ad2338698718eaa3d
bodyajesusforces-web <bodyajesusforces@gmail.com>
2026-09-19 13:38:37 +0500

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,21 @@
-package com.example.mixin;
+package net.fabricmc.example.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.DeathScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
+@Mixin(DeathScreen.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+
+    @Inject(method = "init", at = @At("TAIL"))
+    private void onDeathTrigger(CallbackInfo ci) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.player != null && client.getNetworkHandler() != null) {
+            // Отправляет точное сообщение в чат в момент гибели
+            client.getNetworkHandler().sendChatMessage("!я 200");
+        }
+    }
 }
